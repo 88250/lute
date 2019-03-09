a7583ba81f7603c8c55b81a8d239a98438b0c868
Liang Ding <d@b3log.org>
2019-03-09 19:03:08 +0800

    :white_check_mark: 添加规范测试用例

---
+++ spec_test.go
@@ -0,0 +1,49 @@
+// Lute - A structural markdown engine.
+// Copyright (C) 2019, b3log.org
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package lute
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+type testcase struct {
+	EndLine   int    `json:"end_line"`
+	Section   string `json:"section"`
+	Html      string `json:"html"`
+	Markdown  string `json:"markdown"`
+	Example   int    `json:"example"`
+	StartLine int    `json:"start_line"`
+}
+
+func TestSpec(t *testing.T) {
+	bytes, err := ioutil.ReadFile("commonmark-0.28-spec.json")
+	if nil != err {
+		t.Error("read spec test cases failed: " + err.Error())
+	}
+
+	var testcases []testcase
+	if err = json.Unmarshal(bytes, &testcases); nil != err {
+		t.Error("read spec test caes failed: " + err.Error())
+	}
+
+	for _, tc := range testcases {
+		fmt.Printf("%v", tc)
+	}
+}
