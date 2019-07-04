80d9f7b1730e69ee25769d5f4585608d07943f03
Liang Ding <d@b3log.org>
2019-07-05 01:02:37 +0800

    :building_construction: 调整分隔符栈

--- delimiter.go
+++ delimiter.go
@@ -15,114 +15,25 @@
 
 package lute
 
-type openerBottom struct {
-	typ      string
-	num      int
-	position int
-}
-
 type delimiter struct {
-	node     Node   // the text node point to
-	typ      string // the type of delimiter ([, ![, *, _)
-	num      int    // the number of delimiters
-	active   bool   // whether the delimiter is "active" (all are active to start)
-	canOpen  bool   // whether the delimiter is a potential opener
-	canClose bool   // whether the delimiter is a potential closer
+	node           *Node       // the text node point to
+	typ            itemType   // the type of delimiter ([, ![, *, _)
+	num            int        // the number of delimiters
+	originalNum    int        // the original number of delimiters
+	active         bool       // whether the delimiter is "active" (all are active to start)
+	canOpen        bool       // whether the delimiter is a potential opener
+	canClose       bool       // whether the delimiter is a potential closer
+	previous, next *delimiter // doubly linked list
 }
 
-type delimiterStack struct {
-	elements      []*delimiter
-	openersBottom []*openerBottom
-}
-
-func (s *delimiterStack) matchOpener(e *delimiter) {
-	for i := len(s.elements) - 1; 0 <= i; i-- {
-		t := s.elements[i]
-		if e.typ == t.typ && e.num == t.num {
-			e := &delimiter{}
-			if 1 == e.num {
-				e.node = &Emphasis{NodeType: NodeEmphasis}
-			} else {
-				e.node = &Strong{NodeType: NodeStrong}
-			}
-
-			for j := i + 1; j < len(s.elements); j++ {
-				e.node.Append(s.elements[j].node)
-			}
-			s.elements = s.elements[:i]
-			s.elements = append(s.elements, e)
-		}
+func (d *delimiter) remove(delimiter *delimiter) {
+	if delimiter.previous != nil {
+		delimiter.previous.next = delimiter.next
 	}
-}
-
-func (s *delimiterStack) popMatch(e *delimiter) (elements []*delimiter) {
-	for i := len(s.elements) - 1; 0 <= i; i-- {
-		t := s.elements[i]
-		if e.typ == t.typ && e.num == t.num {
-			elements = append(s.elements[i:], e)
-			s.elements = s.elements[:i]
-
-			return
-		}
+	if delimiter.next == nil {
+		// top of stack
+		d = delimiter.previous
+	} else {
+		delimiter.next.previous = delimiter.previous
 	}
-
-	return nil
-}
-
-func (s *delimiterStack) insert(position int, e *delimiter) {
-	begin := append(s.elements[0:position], e)
-	end := s.elements[position+1:]
-	s.elements = append(begin, end...)
-}
-
-func (s *delimiterStack) removeDelimiters(begin, end int) {
-	for i := begin; i < len(s.elements); i++ {
-		if "*" == s.elements[i].typ {
-			s.remove(i)
-		}
-	}
-}
-
-func (s *delimiterStack) removeRange(begin, end int) {
-	b := s.elements[0:begin]
-	e := s.elements[end:]
-	s.elements = append(b, e...)
-
-}
-
-func (s *delimiterStack) remove(position int) {
-	begin := s.elements[0:position]
-	end := s.elements[position:]
-	s.elements = append(begin, end...)
-}
-
-func (s *delimiterStack) push(e *delimiter) {
-	s.elements = append(s.elements, e)
-}
-
-func (s *delimiterStack) pop() *delimiter {
-	if 0 == len(s.elements) {
-		return nil
-	}
-
-	return s.elements[len(s.elements)-1]
-}
-
-func (s *delimiterStack) popAll() []*delimiter {
-	ret := s.elements
-	s.elements = []*delimiter{}
-
-	return ret
-}
-
-func (s *delimiterStack) peek() *delimiter {
-	if 0 == len(s.elements) {
-		return nil
-	}
-
-	return s.elements[len(s.elements)-1]
-}
-
-func (s *delimiterStack) isEmpty() bool {
-	return 0 == len(s.elements)
 }
