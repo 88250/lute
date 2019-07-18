f6c5786dce215efdd8aa1159c182ec197380f2ef
Liang Ding <d@b3log.org>
2019-07-18 17:36:18 +0800

    :boom: spec285 AST 生成策略用递归下降很容易有冲突

--- parse_test.go
+++ parse_test.go
@@ -29,6 +29,7 @@ type parseTest struct {
 var parseTests = []parseTest{
 	// commonmark spec cases
 
+	{"spec285", "* a\n*\n\n* c\n", "<ul>\n<li>\n<p>a</p>\n</li>\n<li></li>\n<li>\n<p>c</p>\n</li>\n</ul>\n"},
 	{"spec283", "1. a\n\n  2. b\n\n    3. c\n", "<ol>\n<li>\n<p>a</p>\n</li>\n<li>\n<p>b</p>\n</li>\n</ol>\n<pre><code>3. c\n</code></pre>\n"},
 	{"spec282", "- a\n - b\n  - c\n   - d\n    - e\n", "<ul>\n<li>a</li>\n<li>b</li>\n<li>c</li>\n<li>d\n- e</li>\n</ul>\n"},
 	{"spec278", "- foo\n- bar\n\n<!-- -->\n\n- baz\n- bim\n", "<ul>\n<li>foo</li>\n<li>bar</li>\n</ul>\n<!-- -->\n<ul>\n<li>baz</li>\n<li>bim</li>\n</ul>\n"},
