fe70dc6556abd67a33249bcac9422ec691321332
Liang Ding <d@b3log.org>
2019-08-30 22:35:38 +0800

    :sparkles: #2

---
+++ wasm/main.go
@@ -0,0 +1,35 @@
+// Lute - A structured markdown engine.
+// Copyright (c) 2019-present, b3log.org
+//
+// Lute is licensed under the Mulan PSL v1.
+// You can use this software according to the terms and conditions of the Mulan PSL v1.
+// You may obtain a copy of Mulan PSL v1 at:
+//     http://license.coscl.org.cn/MulanPSL
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+// PURPOSE.
+// See the Mulan PSL v1 for more details.
+
+// +build wasm
+
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+
+	"github.com/b3log/lute"
+)
+
+func markdown(this js.Value, args []js.Value) interface{} {
+	markdownText := args[0].String()
+	fmt.Printf("markdown text [%s]", markdownText)
+	luteEngine := lute.New()
+	html, _ := luteEngine.MarkdownStr("", markdownText)
+	return html
+}
+
+func main() {
+	js.Global().Set("markdown", js.FuncOf(markdown))
+	select {}
+}
