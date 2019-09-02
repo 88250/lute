b2cd272b53429c2961104b03a8584c69edb3a22c
Liang Ding <d@b3log.org>
2019-09-02 09:59:13 +0800

    :art: Wasm 条件编译

--- inline.go
+++ inline.go
@@ -10,95 +10,12 @@
 // PURPOSE.
 // See the Mulan PSL v1 for more details.
 
-// +build !wasm
-
 package lute
 
 import (
 	"bytes"
 	"strings"
-	"sync"
 )
-
-// parseInlines 解析并生成行级节点。
-func (t *Tree) parseInlines() {
-	t.walkParseInline(t.Root, nil)
-}
-
-// walkParseInline 解析生成节点 node 的行级子节点。
-func (t *Tree) walkParseInline(node *Node, wg *sync.WaitGroup) {
-	defer recoverPanic(nil)
-	if nil != wg {
-		defer wg.Done()
-	}
-	if nil == node {
-		return
-	}
-
-	// 只有如下几种类型的块节点需要生成行级子节点
-	if typ := node.typ; NodeParagraph == typ || NodeHeading == typ || NodeTableCell == typ {
-		tokens := node.tokens
-		if NodeParagraph == typ && nil == tokens {
-			// 解析 GFM 表节点后段落内容 tokens 可能会被置换为空，具体可参看函数 Paragraph.Finalize()
-			// 在这里从语法树上移除空段落节点
-			next := node.next
-			node.Unlink()
-			// Unlink 会将后一个兄弟节点置空，此处是在在遍历过程中修改树结构，所以需要保持继续迭代后面的兄弟节点
-			node.next = next
-			return
-		}
-
-		length := len(tokens)
-		if 1 > length {
-			return
-		}
-
-		ctx := &InlineContext{
-			tokens:    tokens,
-			tokensLen: length,
-		}
-
-		// 生成该块节点的行级子节点
-		t.parseInline(node, ctx)
-
-		// 处理该块节点中的强调、加粗和删除线
-		t.processEmphasis(nil, ctx)
-
-		// 将连续的文本节点进行合并。
-		// 规范只是定义了从输入的 Markdown 文本到输出的 HTML 的解析渲染规则，并未定义中间语法树的规则。
-		// 也就是说语法树的节点结构没有标准，可以自行发挥。这里进行文本节点合并主要有两个目的：
-		// 1. 减少节点数量，提升后续处理性能
-		// 2. 方便后续功能方面的处理，比如 GFM 自动链接解析
-		t.mergeText(node)
-
-		if t.context.option.GFMAutoLink {
-			t.parseGFMAutoEmailLink(node)
-			t.parseGFMAutoLink(node)
-		}
-
-		if t.context.option.AutoSpace {
-			t.space(node)
-		}
-
-		if t.context.option.FixTermTypo {
-			t.fixTermTypo(node)
-		}
-
-		if t.context.option.Emoji {
-			t.emoji(node)
-		}
-
-		return
-	}
-
-	// 遍历处理子节点，通过并行处理提升性能
-	cwg := &sync.WaitGroup{}
-	for child := node.firstChild; nil != child; child = child.next {
-		cwg.Add(1)
-		go t.walkParseInline(child, cwg)
-	}
-	cwg.Wait()
-}
 
 // parseInline 解析并生成块节点 block 的行级子节点。
 func (t *Tree) parseInline(block *Node, ctx *InlineContext) {
