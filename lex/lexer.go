a5e0b0a7d7c1245ad11afb92ec1474095bf3fa3c
Liang Ding <d@b3log.org>
2020-02-16 15:13:18 +0800

    :recycle: 合并源码文件

--- lex/lexer.go
+++ lex/lexer.go
@@ -10,6 +10,8 @@
 
 package lex
 
+import "unicode/utf8"
+
 // Lexer 描述了词法分析器结构。
 type Lexer struct {
 	input  []byte // 输入的文本字节数组
@@ -28,5 +30,53 @@ func NewLexer(input []byte) (ret *Lexer) {
 		ret.input = append(ret.input, ItemNewline)
 		ret.length++
 	}
+	return
+}
+
+// NextLine 返回下一行。
+func (l *Lexer) NextLine() (ret []byte) {
+	if l.offset >= l.length {
+		return
+	}
+
+	var b, nb byte
+	i := l.offset
+	for ; i < l.length; i += l.width {
+		b = l.input[i]
+		if ItemNewline == b {
+			i++
+			break
+		} else if ItemCarriageReturn == b {
+			// 处理 \r
+			if i < l.length-1 {
+				nb = l.input[i+1]
+				if ItemNewline == nb {
+					l.input = append(l.input[:i], l.input[i+1:]...) // 移除 \r，依靠下一个的 \n 切行
+					l.length--                                      // 重新计算总长
+				}
+			}
+			i++
+			break
+		} else if '\u0000' == b {
+			// 将 \u0000 替换为 \uFFFD
+			l.input = append(l.input, 0, 0)
+			copy(l.input[i+2:], l.input[i:])
+			// \uFFFD 的 UTF-8 编码为 \xEF\xBF\xBD 共三个字节
+			l.input[i] = '\xEF'
+			l.input[i+1] = '\xBF'
+			l.input[i+2] = '\xBD'
+			l.length += 2 // 重新计算总长
+			l.width = 3
+			continue
+		}
+
+		if utf8.RuneSelf <= b { // 说明占用多个字节
+			_, l.width = utf8.DecodeRune(l.input[i:])
+		} else {
+			l.width = 1
+		}
+	}
+	ret = l.input[l.offset:i]
+	l.offset = i
 	return
 }
