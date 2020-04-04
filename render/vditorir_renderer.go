f69c8d810b7ebde5b9c121224280d005222d2a8f
Liang Ding <d@b3log.org>
2020-04-04 22:28:13 +0800

    :bug: Vditor 支持 linkToImgUrl 图片上传问题 https://github.com/Vanessa219/vditor/issues/266

--- render/vditorir_renderer.go
+++ render/vditorir_renderer.go
@@ -588,7 +588,13 @@ func (r *VditorIRRenderer) renderImage(node *ast.Node, entering bool) ast.WalkSt
 		r.tag("span", [][]string{{"class", "vditor-ir__node"}}, false)
 	} else {
 		dest := node.ChildByType(ast.NodeLinkDest)
-		r.WriteString("<img src=\"" + string(dest.Tokens) + "\">")
+		attrs := [][]string{{"src", string(dest.Tokens)}}
+		alt := node.ChildByType(ast.NodeLinkText)
+		if nil != alt && 0 < len(alt.Tokens) {
+			altTokens := bytes.ReplaceAll(alt.Tokens, []byte(parse.Caret), nil)
+			attrs = append(attrs, []string{"alt", string(altTokens)})
+		}
+		r.tag("img", attrs, true)
 		r.tag("/span", nil, false)
 	}
 	return ast.WalkContinue
