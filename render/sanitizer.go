746674c423a4ed4886811f2b8bea8dcfbd5ce3e0
Liang Ding <d@b3log.org>
2020-06-18 09:22:27 +0800

    :art: Vditor 支持 行级 HTML 空格问题 https://github.com/Vanessa219/vditor/issues/496

--- render/sanitizer.go
+++ render/sanitizer.go
@@ -189,6 +189,101 @@ func sanitizeAttrs(attrs []html.Attribute) (ret []html.Attribute) {
 }
 
 func allowAttr(attrName string) bool {
-	// https://www.w3schools.com/tags/ref_eventattributes.asp
-	return !strings.HasPrefix(attrName, "on")
+	for name := range eventAttrs {
+		if attrName == name {
+			return false
+		}
+	}
+	return true
+}
+
+// HTML 事件属性。https://www.w3schools.com/tags/ref_eventattributes.asp
+var eventAttrs = map[string]interface{}{
+	// Window
+	"onafterprint":   nil,
+	"onbeforeprint":  nil,
+	"onbeforeunload": nil,
+	"onerror":        nil,
+	"onhashchange":   nil,
+	"onload":         nil,
+	"onmessage":      nil,
+	"onoffline":      nil,
+	"ononline":       nil,
+	"onpagehide":     nil,
+	"onpageshow":     nil,
+	"onpopstate":     nil,
+	"onresize":       nil,
+	"onstorage":      nil,
+	"onunload":       nil,
+
+	// Form
+	"onblur":        nil,
+	"onchange":      nil,
+	"oncontextmenu": nil,
+	"onfocus":       nil,
+	"oninput":       nil,
+	"oninvalid":     nil,
+	"onreset":       nil,
+	"onsearch":      nil,
+	"onselect":      nil,
+	"onsubmit":      nil,
+
+	// Keyboard
+	"onkeydown":  nil,
+	"onkeypress": nil,
+	"onkeyup":    nil,
+
+	// Mouse
+	"onclick":      nil,
+	"ondblclick":   nil,
+	"onmousedown":  nil,
+	"onmousemove":  nil,
+	"onmouseout":   nil,
+	"onmouseover":  nil,
+	"onmouseup":    nil,
+	"onmousewheel": nil,
+	"onwheel":      nil,
+
+	// Drag
+	"ondrag":      nil,
+	"ondragend":   nil,
+	"ondragenter": nil,
+	"ondragleave": nil,
+	"ondragover":  nil,
+	"ondragstart": nil,
+	"ondrop":      nil,
+	"onscroll":    nil,
+
+	// Clipboard
+	"oncopy":  nil,
+	"oncut":   nil,
+	"onpaste": nil,
+
+	// Media
+	"onabort":          nil,
+	"oncanplay":        nil,
+	"oncanplaythrough": nil,
+	"oncuechange":      nil,
+	"ondurationchange": nil,
+	"onemptied":        nil,
+	"onended":          nil,
+	//"onerror":  nil,
+	"onloadeddata":     nil,
+	"onloadedmetadata": nil,
+	"onloadstart":      nil,
+	"onpause":          nil,
+	"onplay":           nil,
+	"onplaying":        nil,
+	"onprogress":       nil,
+	"onratechange":     nil,
+	"onseeked":         nil,
+	"onseeking":        nil,
+	"onstalled":        nil,
+	"onsuspend":        nil,
+	"ontimeupdate":     nil,
+	"onvolumechange":   nil,
+	"onwaiting":        nil,
+
+	// Misc
+	"ontoggle": nil,
 }
