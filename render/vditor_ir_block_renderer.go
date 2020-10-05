35d2ee95af417e85e7294f8fbbccf4daa4d80c87
Liang Ding <d@b3log.org>
2020-10-05 22:54:58 +0800

    :art: 思源笔记支持

--- render/vditor_ir_block_renderer.go
+++ render/vditor_ir_block_renderer.go
@@ -613,10 +613,10 @@ func (r *VditorIRBlockRenderer) renderInlineMathContent(node *ast.Node, entering
 	r.tag("/code", nil, false)
 	r.tag("span", [][]string{{"class", "vditor-ir__preview"}, {"data-render", "2"}}, false)
 	r.tag("code", [][]string{{"class", "language-math"}}, false)
+	r.tag("span", [][]string{{"class", "vditor-math"}}, false)
 	tokens = bytes.ReplaceAll(tokens, util.CaretTokens, nil)
 	r.Write(tokens)
-	r.tag("/code", nil, false)
-	r.tag("/span", nil, false)
+	r.WriteString("</span></code></span>")
 	return ast.WalkStop
 }
 
