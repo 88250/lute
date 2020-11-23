26573f74f62e9bece5f8dc5ecbdeb96fa66033c3
Liang Ding <d@b3log.org>
2020-11-23 19:00:30 +0800

    :art: 增加测试用例 https://github.com/88250/lute/issues/114

---
+++ test/block_query_embed_test.go
@@ -0,0 +1,33 @@
+// Lute - 一款对中文语境优化的 Markdown 引擎，支持 Go 和 JavaScript
+// Copyright (c) 2019-present, b3log.org
+//
+// Lute is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//         http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+package test
+
+import (
+	"testing"
+
+	"github.com/88250/lute"
+)
+
+var blockQueryEmbedTests = []parseTest{
+
+	{"0", "foo\n!{{ SELECT * FROM blocks }}\nbar", "<p>foo</p>\n<div>\"SELECT * FROM blocks\"</div>\n<p>bar</p>\n"},
+}
+
+func TestBlockQueryEmbed(t *testing.T) {
+	luteEngine := lute.New()
+	luteEngine.BlockRef = true
+	for _, test := range blockQueryEmbedTests {
+		html := luteEngine.MarkdownStr(test.name, test.from)
+		if test.to != html {
+			t.Fatalf("test case [%s] failed\nexpected\n\t%q\ngot\n\t%q\noriginal markdown text\n\t%q", test.name, test.to, html, test.from)
+		}
+	}
+}
