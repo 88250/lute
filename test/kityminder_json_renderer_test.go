8cabc1d0d793975bfb8e8b8f2b3f2d3f7bdf9ab2
Liang Ding <d@b3log.org>
2021-01-10 10:56:22 +0800

    :sparkles: KityMinder JSON 渲染器 https://github.com/88250/lute/issues/127

--- test/kityminder_json_renderer_test.go
+++ test/kityminder_json_renderer_test.go
@@ -11,6 +11,7 @@
 package test
 
 import (
+	"github.com/88250/lute/ast"
 	"testing"
 
 	"github.com/88250/lute"
@@ -18,15 +19,16 @@ import (
 
 var kitymindJSONRendererTests = []parseTest{
 
-	{"2", "foo\nbar", "{\"root\":{\"data\":{\"text\":\"文档名 TODO\"},\"children\":[{\"data\":{\"text\":\"foo\\nbar\"},\"children\":[]}]}}"},
-	{"1", "# foo\n\n para1\n\npara2", "{\"root\":{\"data\":{\"text\":\"文档名 TODO\"},\"children\":[{\"data\":{\"text\":\"# foo\"},\"children\":[{\"data\":{\"text\":\"para1\"},\"children\":[]},{\"data\":{\"text\":\"para2\"},\"children\":[]}]}]}}"},
-	{"0", "foo **bar**\n", "{\"root\":{\"data\":{\"text\":\"文档名 TODO\"},\"children\":[{\"data\":{\"text\":\"foo **bar**\"},\"children\":[]}]}}"},
+	{"2", "foo\nbar", "{\"root\":{\"data\":{\"text\":\"文档名 TODO\",\"id\":\"20060102150405-1a2b3c4\"},\"children\":[{\"data\":{\"text\":\"foobar\",\"id\":\"\"},\"children\":[]}]}}"},
+	{"1", "# foo\n\n para1\n\npara2", "{\"root\":{\"data\":{\"text\":\"文档名 TODO\",\"id\":\"20060102150405-1a2b3c4\"},\"children\":[{\"data\":{\"text\":\"# foo\",\"id\":\"\"},\"children\":[{\"data\":{\"text\":\"para1\",\"id\":\"\"},\"children\":[]},{\"data\":{\"text\":\"para2\",\"id\":\"\"},\"children\":[]}]}]}}"},
+	{"0", "foo **bar**\n", "{\"root\":{\"data\":{\"text\":\"文档名 TODO\",\"id\":\"20060102150405-1a2b3c4\"},\"children\":[{\"data\":{\"text\":\"foo **bar**\",\"id\":\"\"},\"children\":[]}]}}"},
 }
 
 func TestKityMinderJSONRenderer(t *testing.T) {
 	luteEngine := lute.New()
 	luteEngine.ParseOptions.KramdownIAL = true
 
+	ast.Testing = true
 	for _, test := range kitymindJSONRendererTests {
 		jsonStr := luteEngine.RenderKityMinderJSON(test.from)
 		t.Log(jsonStr)
