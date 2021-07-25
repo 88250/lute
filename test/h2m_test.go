aa6519bea1068b593b1e6cec8a8fd54a28253dc6
Liang Ding <d@b3log.org>
2021-07-25 10:05:57 +0800

    :bug: 修复链接中包含标题时 HTMl 转 Markdown 问题 https://github.com/siyuan-note/siyuan/issues/2498

--- test/h2m_test.go
+++ test/h2m_test.go
@@ -18,6 +18,7 @@ import (
 
 var html2MdTests = []parseTest{
 
+	{"49", "<a href=\"https://b3log.org/siyuan\" data-ved=\"2ahUKEwiE1_PRiP3xAhXQtp4KHXw1AcMQFjAAegQIBhAD\" ping=\"/url?sa=t&amp;source=web&amp;rct=j&amp;url=https://b3log.org/siyuan&amp;ved=2ahUKEwiE1_PRiP3xAhXQtp4KHXw1AcMQFjAAegQIBhAD\"><h3 class=\"LC20lb DKV0Md\">思源笔记- 本地优先的个人知识管理系统，支持Markdown 排版 ...</h3></a>", "[思源笔记- 本地优先的个人知识管理系统，支持 Markdown 排版 ...](https://b3log.org/siyuan)\n"},
 	{"48", "<figure data-size=\"normal\"><img src=\"foo.png\"></figure><h2>bar</h2>", "![](foo.png)\n\n## bar\n"},
 	{"47", "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"207\">\n <colgroup><col width=\"207\">\n </colgroup><tbody><tr height=\"126\">\n\n  <td height=\"126\" class=\"xl66\" width=\"207\">foo<font class=\"font7\">bar</font><font class=\"font6\">；<br>\n    baz</font></td>\n\n </tr>\n</tbody></table>", "| foobar；    baz |\n| ----------------- |\n"},
 	{"46", "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"207\">\n <colgroup><col width=\"207\">\n </colgroup><tbody><tr height=\"180\">\n\n  <td height=\"180\" class=\"xl66\" width=\"207\"><font class=\"font6\">foo<br>\n    </font><font class=\"font7\">bar</font></td>\n\n </tr>\n</tbody></table>", "| foo    bar |\n| ------------ |\n"},
