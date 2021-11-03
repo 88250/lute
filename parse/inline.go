4e727db6009eb9d94458b70d4e22e386bf6f8d6c
Liang Ding <d@b3log.org>
2021-11-03 10:36:44 +0800

    :bug: 超链接、块引用、PDF 标注锚文本转义问题 https://github.com/siyuan-note/siyuan/issues/3320

--- parse/inline.go
+++ parse/inline.go
@@ -47,6 +47,7 @@ func (t *Tree) parseInline(block *ast.Node, ctx *InlineContext) {
 						n = t.parseInlineHTML(ctx)
 						if t.Context.ParseOption.ProtyleWYSIWYG && nil != n && ast.NodeInlineHTML == n.Type {
 							// Protyle 中不存在内联 HTML，使用文本
+							n.Tokens = html.EscapeHTML(n.Tokens)
 							n.Type = ast.NodeText
 						}
 					}
