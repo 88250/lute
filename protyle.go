6f1ed4c3a0b50b3c78b022801e8caddf85eba429
Liang Ding <d@b3log.org>
2023-02-27 13:28:17 +0800

    :bug: Protyle 外部内容粘贴到表格中后编辑导致换行丢失 https://github.com/siyuan-note/siyuan/issues/7501

--- protyle.go
+++ protyle.go
@@ -1011,8 +1011,10 @@ func (lute *Lute) genASTContenteditable(n *html.Node, tree *parse.Tree) {
 		}
 
 		if lute.parentIs(n, atom.Table) {
-			if nil == n.NextSibling || (atom.Br == n.NextSibling.DataAtom && strings.HasPrefix(content, "\n")) {
-				content = strings.ReplaceAll(content, "\n", "") // 表格内存在行级公式时编辑会产生换行 https://github.com/siyuan-note/siyuan/issues/2279
+			content = strings.TrimSpace(content)
+			if (nil == n.NextSibling && !strings.Contains(content, "\n")) /* 外部内容粘贴到表格中后编辑导致换行丢失  https://github.com/siyuan-note/siyuan/issues/7501 */ ||
+				(nil != n.NextSibling && atom.Br == n.NextSibling.DataAtom && strings.HasPrefix(content, "\n")) /* 表格内存在行级公式时编辑会产生换行 https://github.com/siyuan-note/siyuan/issues/2279 */ {
+				content = strings.ReplaceAll(content, "\n", "")
 			}
 
 			node.Tokens = util.StrToBytes(strings.ReplaceAll(content, "\n", "<br />"))
