c1a474f3978e7b0b6e0bb8d8acd981d92f3cf009
Daniel <845765@qq.com>
2024-06-24 17:11:43 +0800

    :art: Improve Wikipedia clipping https://github.com/siyuan-note/siyuan/issues/11640

--- util/html.go
+++ util/html.go
@@ -167,7 +167,6 @@ func domTexhtml0(n *html.Node, buffer *bytes.Buffer) {
 	switch n.DataAtom {
 	case 0:
 		buffer.WriteString(escapeMathSymbol(n.Data))
-		return
 	case atom.Sup:
 		buffer.WriteString("^")
 	case atom.Sub:
@@ -176,6 +175,13 @@ func domTexhtml0(n *html.Node, buffer *bytes.Buffer) {
 
 	for child := n.FirstChild; nil != child; child = child.NextSibling {
 		domTexhtml0(child, buffer)
+	}
+
+	switch n.DataAtom {
+	case atom.Sup:
+		buffer.WriteString("^")
+	case atom.Sub:
+		buffer.WriteString("_")
 	}
 }
 
