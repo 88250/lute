51e2202b5bb27ff070d6937178641419957a9706
Daniel <845765@qq.com>
2024-09-05 22:40:01 +0800

    :art: Protyle Improve code block preview https://github.com/siyuan-note/siyuan/issues/12394

--- render/protyle_preview_renderer.go
+++ render/protyle_preview_renderer.go
@@ -710,6 +710,15 @@ func (r *ProtylePreviewRenderer) renderCodeBlock(node *ast.Node, entering bool) 
 		}
 
 		attrs := [][]string{{"class", "code-block"}, {"data-language", language}}
+		if "true" == node.IALAttr("linewrap") {
+			attrs = append(attrs, []string{"linewrap", "true"})
+		}
+		if "true" == node.IALAttr("ligatures") {
+			attrs = append(attrs, []string{"ligatures", "true"})
+		}
+		if "true" == node.IALAttr("linenumber") {
+			attrs = append(attrs, []string{"linenumber", "true"})
+		}
 		r.Tag("pre", attrs, false)
 		r.WriteString("<code class=\"hljs\">")
 	} else {
