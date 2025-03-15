51571fa9286c9873f0f5b98880fe26e7e5eaa646
Daniel <845765@qq.com>
2025-03-15 10:35:16 +0800

    :art: Improve pasting formulas from Claude https://github.com/siyuan-note/siyuan/issues/14360

--- h2m.go
+++ h2m.go
@@ -176,6 +176,13 @@ func (lute *Lute) genASTByDOM(n *html.Node, tree *parse.Tree) {
 			}
 		}
 
+		if ast.NodeParagraph == tree.Context.Tip.Type && bytes.HasPrefix(node.Tokens, []byte("$$")) && bytes.HasSuffix(node.Tokens, []byte("$$")) {
+			// 将其转换为公式块 https://github.com/siyuan-note/siyuan/issues/14360
+			tex := bytes.TrimSpace(node.Tokens[2 : len(node.Tokens)-2])
+			appendMathBlock(tree, util.BytesToStr(tex))
+			return
+		}
+
 		if nil != n.Parent && atom.Span == n.Parent.DataAtom && 0 == len(n.Parent.Attr) {
 			// 按原文解析，不处理转义
 		} else {
